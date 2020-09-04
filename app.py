826bfd32ca456594596d2fae764253e6b7a6c071
Washington Barbosa <34190079+Washingtonban@users.noreply.github.com>
2020-09-03 22:31:25 -0400

    Feature/app api (#3)

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import flask
 from flask import jsonify, request
-
-
-from model.data_connect import *
+from model.route_search import RouteSearch
+from model.data_connect import DataConnect
 
 
 app = flask.Flask(__name__)
@@ -11,15 +10,28 @@ app.config["DEBUG"] = True
 
 @app.route("/api/update", methods=["POST"])
 def update():
+    model = DataConnect()
     json_response = request.get_json()
-    return jsonify(json_response)
+    result = model.updateCsv(data=json_response['data'], line=json_response['line'])
+    return jsonify({"message": "New route successfully created"})
 
 
 
 @app.route("/api/route", methods=["POST"])
 def route():
+
+    model = RouteSearch()
     json_response = request.get_json()
-    return jsonify(json_response)
+    result = model.route_search(origem=json_response['origem'], destino=json_response['destino'])
+
+    final = ''
+    for item in result[1]:
+        if item == result[1][-1]:
+            final += f' {item}'
+        else:
+            final += f' {item} -'
+
+    return f'{final} ao custo de ${result[0]}'
 
 
 if __name__ == '__main__':
